12cb960ff5c4eef994ca6050e91fe48986819f4b
Ranariel <ranarieldossantos@gmail.com>
2026-09-03 11:15:22 -0300

    fix: adicionar FriendsView.xaml.cs (InitializeComponent faltando)

---
+++ JaClipei.Client/Views/FriendsView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace JaClipei.Client.Views;
+
+public partial class FriendsView : UserControl
+{
+    public FriendsView()
+    {
+        InitializeComponent();
+    }
+}
